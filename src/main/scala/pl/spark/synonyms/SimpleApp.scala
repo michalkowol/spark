fd054a7dd6545dce613a74cf825402b055a14e5c
vagrant <vagrant@spark-standalone-cluster>
2015-01-15 19:21:40 +0000

    Counting frequency based on Pawel work

--- src/main/scala/pl/spark/synonyms/SimpleApp.scala
+++ src/main/scala/pl/spark/synonyms/SimpleApp.scala
@@ -6,12 +6,15 @@ import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = args(0)
+    val file = args(0)
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    val wordDataFile = sc.textFile(file, 2).cache()
+    val frequency = wordDataFile.flatMap(line => line.split("\\W+"))
+    							.map(word => (word.toLowerCase, 1))
+    							.reduceByKey((a, b) => a + b).map { case (word, count) => (count, word) } 
+    							.sortByKey(false)
+    							.collect()
+    frequency.take(10).foreach(println)
   }
 }

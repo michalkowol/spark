f16c1eec9129887914b6404414784a1520ed6c77
Michal Kowol <michal.kowol@vimn.com>
2015-01-15 20:04:09 +0100

    NT Synonyms

--- src/main/scala/pl/spark/synonyms/Synonyms.scala
+++ src/main/scala/pl/spark/synonyms/Synonyms.scala
@@ -1,3 +1,5 @@
+package pl.spark.synonyms
+
 trait Synonyms {
   def synonyms(word: String): Seq[String]
 }
---
+++ src/main/scala/pl/spark/synonyms/WordnetSynonyms.scala
@@ -0,0 +1,5 @@
+package pl.spark.synonyms
+
+class WordnetSynonyms extends Synonyms {  
+  def synonyms(word: String): Seq[String] = ???
+}

a0aaca08316de60f6d2d8524ad1f65340b838d5d
Michal Kowol <michal.kowol@vimn.com>
2015-01-15 21:32:57 +0100

    NT Minor changes

--- src/main/scala/pl/spark/synonyms/WordnetSynonyms.scala
+++ src/main/scala/pl/spark/synonyms/WordnetSynonyms.scala
@@ -4,7 +4,7 @@ import edu.smu.tspell.wordnet._
 import scala.collection.JavaConversions._
 import scala.util.control.NonFatal
 
-class WordnetSynonyms extends Synonyms {
+class WordnetSynonyms(path: String) extends Synonyms {
 
   val database = WordNetDatabase.getFileInstance()
 
@@ -25,13 +25,13 @@ class WordnetSynonyms extends Synonyms {
   }
 
   private def createWordNetDatabase: WordNetDatabase = {
-    System.setProperty("wordnet.database.dir", "/Users/kowolm/tmp/spark/wordnet/dict")
+    System.setProperty("wordnet.database.dir", path)
     val database = WordNetDatabase.getFileInstance()
     database
   }
 }
 
 object WordnetApp extends App {
-  val synonyms = new WordnetSynonyms().synonyms("go")
+  val synonyms = new WordnetSynonyms("/Users/kowolm/tmp/spark/wordnet/dict").synonyms("go")
   println(synonyms)
 }
